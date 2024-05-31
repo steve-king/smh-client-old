e566647667821b04b65609b5efee99a6e539fe0e
steve-king <steve,king5891@gmail.com>
2024-05-31 23:29:47 +0100

    Debugging - adding table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,9 @@ export default function Home() {
     <main className="container py-4">
       <Card>
         <CardHeader>Nodes</CardHeader>
-        <CardContent>{/* <Nodes /> */}</CardContent>
+        <CardContent>
+          <Nodes />
+        </CardContent>
       </Card>
     </main>
   )

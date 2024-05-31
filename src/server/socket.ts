b118160815b743b0a82ede7d000bef1069a65a0c
steve-king <steve,king5891@gmail.com>
2024-05-31 16:48:42 +0100

    tidy up

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -13,31 +13,21 @@ class SocketServer {
   }
 
   init = (server: Server) => {
-    this.io = new SocketIO.Server(server, {
-      cors: {
-        origin: '*',
-      },
-    })
-
-    this.io.on('connection', (socket: Socket) => {
-      const ip = socket.handshake.address
-
-      log('INFO', 'socket', `client connected: ${ip}`)
-
-      socket.on('disconnect', () => {
-        log('INFO', 'socket', `client disconnected: ${ip}`)
-      })
-    })
+    this.io = new SocketIO.Server(server)
 
     log('INFO', 'socket', 'initialised')
 
-    this.io.engine.on('connection_error', (err: any) => {
-      // console.log(err.req) // the request object
-      // console.log(err.code) // the error code, for example 1
-      console.log(err.message) // the error message, for example "Session ID unknown"
+    this.io.on('connection', (socket: Socket) => {
+      const ip = socket.handshake.address
+      log('INFO', 'socket', `client connected: ${ip}`)
 
-      log('ERROR', 'socket', err.code + ' ' + err.message + ' ' + err.context)
-      // console.log(err.context) // some additional error context
+      socket.on('disconnect', () =>
+        log('INFO', 'socket', `client disconnected: ${ip}`)
+      )
+    })
+
+    this.io.engine.on('connection_error', (err: any) => {
+      log('ERROR', 'socket', err.code + '|' + err.message + '|' + err.context)
     })
 
     return this
